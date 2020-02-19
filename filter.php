8570463f1bc58cffb3a732368e658bebf4034432
Praveen Dangi <praveen.spidrontech@gmail.com>
2020-02-19 19:05:38 +0530

    minimum and max range

--- filter.php
+++ filter.php
@@ -24,6 +24,24 @@
      $vemail = filter_var($email,FILTER_SANITIZE_EMAIL);
      echo $vemail;
     ?>
+
+    <!-- value witin and maximum range -->
+
+    <?php
+/* variable to check */
+        $int = 122;
+
+/* min value */
+        $min = 1;
+/* max value */
+        $max = 200;
+
+        if (filter_var($int, FILTER_VALIDATE_INT, array("options" => array("min_range"=>$min, "max_range"=>$max))) === false) {
+        echo("Variable value is not within the legal range");
+        } else {
+        echo("Variable value is within the legal range");
+        }
+?>  
    
 
 </body>
